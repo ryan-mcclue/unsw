5c5c405ed481eaa6832b10337e57bd1f217d8ea1
Ryan McClue <re.mcclue@protonmail.com>
2022-02-15 19:58:03 +1100

    Add notes on diagramming

--- comp3331/week1.py
+++ comp3331/week1.py
@@ -1,15 +1,20 @@
 # SPDX-License-Identifier: zlib-acknowledgement
 
-# IMPORTANT(Ryan):  
-# General:  ssh z5346008@login.cse.unsw.edu.au
-# Specific to course: ssh z5346008@d.cse.unsw.edu.au
+# IMPORTANT(Ryan): Various UNSW CompSci servers:
+# General ➞  ssh z5346008@login.cse.unsw.edu.au
+# Course ➞  ssh z5346008@d.cse.unsw.edu.au
 # For Vlab, access the vnc server
 
-# NOTE(Ryan): We are using Postgresql as it has better support for the SQL language, with Mysql utilising many extensions.
-# postgresql is client-server based (unlike sqlite3)
-
-# TODO(Ryan): Investigate advanced SQL topics: views, stored procedures, triggers, aggregates
+# NOTE(Ryan): We are using Postgresql as it has better support for the SQL language (MySQL utilises too many non-standard extensions)
+# It's client-server based (unlike sqlite3).
 
 # IMPORTANT(Ryan): RDBMS involves two structuring mechanisms:
 #  1. Relation: object name, e.g. Movie, Actor
 #  2. Tuple: key value pair
+  
+# er diagram relationships with multiplicity. db schema diagram shows primary and foreign keys.
+# for multiplicity we may have to introduce relations representing relationships, e.g. BelongsTo, AppearsIn, etc.
+
+# will have a schema.sql file and a data.sql file containing table inserts
+
+# TODO(Ryan): Investigate advanced SQL topics: views, stored procedures, triggers, aggregates

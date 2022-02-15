30770cc1b299ef69a7e74df81bd641636d2a0ec3
Ryan McClue <re.mcclue@protonmail.com>
2022-02-15 16:05:20 +1100

    Add initial psql notes

---
+++ comp3331/week1.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: zlib-acknowledgement
+
+# IMPORTANT(Ryan):  
+# General:  ssh z5346008@login.cse.unsw.edu.au
+# Specific to course: ssh z5346008@d.cse.unsw.edu.au
+# For Vlab, access the vnc server
+
+# NOTE(Ryan): We are using Postgresql as it has better support for the SQL language, with Mysql utilising many extensions.
+# postgresql is client-server based (unlike sqlite3)
+
+# TODO(Ryan): Investigate advanced SQL topics: views, stored procedures, triggers, aggregates
+
+# IMPORTANT(Ryan): RDBMS involves two structuring mechanisms:
+#  1. Relation: object name, e.g. Movie, Actor
+#  2. Tuple: key value pair

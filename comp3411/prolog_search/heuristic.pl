c4a706ba67b99d9b44d1041e6d5c99823b2777bc
Ryan McClue <re.mcclue@protonmail.com>
2022-02-25 18:21:11 +1100

    Add 'w' value for ai assignment

--- comp3411/prolog_search/heuristic.pl
+++ comp3411/prolog_search/heuristic.pl
@@ -40,6 +40,6 @@ depthlim(Path, Node, G, F_limit, Sol, G2)  :-
     not(member(Node1, Path)),      % Prevent a cycle
     G1 is G + C,
     h(Node1, H1),
-    F1 is G1 + H1,
+    F1 is (2 - 1.2) * G1 + (1.2 * H1), % (2 - w)·g(n) + w·f(n)
     F1 =< F_limit,
     depthlim([Node|Path], Node1, G1, F_limit, Sol, G2).

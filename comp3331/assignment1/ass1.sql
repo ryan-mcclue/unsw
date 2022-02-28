5dbf6d0094ae2b6ec9db0b15be85e552d6d8136e
Ryan McClue <re.mcclue@protonmail.com>
2022-02-28 18:57:47 +1100

    Add db assignment files

---
+++ comp3331/assignment1/ass1.sql
@@ -0,0 +1,83 @@
+-- comp3311 22T1 Assignment 1
+
+-- Q1
+create or replace view Q1(unswid, name)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+
+-- Q2
+create or replace view Q2(unswid, name, course_cnt)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+
+-- Q3
+create or replace view Q3(unswid, name)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+
+-- Q4
+create or replace view Q4(unswid, name)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+
+-- Q5a
+create or replace view Q5a(term, min_fail_rate)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+
+-- Q5b
+create or replace view Q5b(term, min_fail_rate)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+
+-- Q6
+create or replace function 
+	Q6(id integer,code text) returns integer
+as $$
+--... SQL statements, possibly using other views/functions defined by you ...
+$$ language sql;
+
+
+-- Q7
+create or replace function 
+	Q7(year integer, session text) returns table (code text)
+as $$
+--... SQL statements, possibly using other views/functions defined by you ...
+$$ language sql;
+
+
+-- Q8
+create or replace function
+	Q8(zid integer) returns setof TermTranscriptRecord
+as $$
+--... SQL statements, possibly using other views/functions defined by you ...
+$$ language plpgsql;
+
+
+-- Q9
+create or replace function 
+	Q9(gid integer) returns setof AcObjRecord
+as $$
+--... SQL statements, possibly using other views/functions defined by you ...
+$$ language plpgsql;
+
+
+-- Q10
+create or replace function
+	Q10(code text) returns setof text
+as $$
+--... SQL statements, possibly using other views/functions defined by you ...
+$$ language plpgsql;
+

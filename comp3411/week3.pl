07ffab1179ca815b38fae15a6c0295490e8cadbf
Ryan McClue <re.mcclue@protonmail.com>
2022-03-07 16:09:33 +1100

    Wk3 ai lecture notes

--- comp3411/week3.pl
+++ comp3411/week3.pl
@@ -24,14 +24,39 @@
 % prolog a subset of first-order logic (which is just like set membership operators, e.g. member of, subclass of, etc.) 
 % in prolog :- can be thought as meaning 'if'. First-order logic is just quantifying membership?
 
-% in logic, a variable is not a memory location. TODO: watch for 1:45 definition of prolog variable
-% think of 'there exists a variable such that it satisfies this condition'
+% in logic, a variable is not a memory location. 
+% think of like a representation of an instance; 'there exists a variable such that it satisfies this condition'
+% it allows for pattern matching
 
 % static fact is something that persists. transient fact only applies at a specific instance
 
 % semantic network; like object orientated programming making a distinction between classes and instances
+% the type of knowledge is say ask for a property on an object, if it doesn't have it, get from parent (in fact object orientation taken from AI knowledge representation; hence why bad for most other things)
+% it takes more work to reason this with pure logic
 % nodes will be objects/values, e.g. car, vehicle, red, 4
 % arcs will be attributes, e.g. 'is a', 'kind of', 'number of wheels'
-% To reason, a semantic network could use frames (code that is run instances added, modified, etc.) 
+% If semantic network requires non-inheritance reasoning, requires procedural attachments. 
+% e.g, could use frames (code that is run when instances added, modified, deleted etc.) 
 
-% rule based system; constantly scanning 
+% rule based system; structured like PNF programming grammar (not actual if else statements)
+% inserting a fact could cause a rule to fire which could cause another rule to fire or produce a derived fact.
+
+% in the knowledge representations covered, they assume no uncertainty.
+% to cpature uncertainty could use bayesian inference (attaching some probability to a fact)
+
+% to prevent having to continously loop over all the rules (which would degrade performance),
+% can compile rules into an inference network. in the situation of duplicate conditions across multiple rules,
+% this will avoid having to iterate over the duplicated subcondition multiple times.
+
+% from facts we can make conclusions,
+% deduction: from cause, e.g. Ryan has strong glutes, get effect, e.g. Ryan is happy
+% abduction: from effect, get cause (useful in diagnosing medical, psychology, aircraft faults)
+% induction: from cause and effect, get rule (machine learning; this is popular as it can be tedious to manually construct all rules)
+
+% everything outside of knowledge base is assumed false. 
+% so more accurate to say can you prove this with your knowledge base, i.e. is this provable or not provable as oppose to true or false
+
+% context is important in knowledge representation, e.g. how many rabbits? does a statue of a rabbit count, etc.
+
+% more nerve endings from brain to eye, then from eye to brain (this is so we can predict things; we can get fooled)
+% do we want cars that are 100% reliable or just more reliable than a human driver?

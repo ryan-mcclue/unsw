c656778f4d55ca7ffed73bf7f2931fba49135c5c
Ryan McClue <re.mcclue@protonmail.com>
2022-09-19 13:01:50 +1000

    lab

---
+++ comp3222/wk2/lab01.vhd
@@ -0,0 +1,13 @@
+-- archive 3,4,5
+-- .jpeg 4 and 5
+
+-- user manual for pin assignments  
+-- reference manual also
+
+-- can target entire vectors
+
+-- segment display 1 indicates particular segment is on
+
+-- mcclue-5346008-lab01-part1.qad
+
+-- testbenches is umbrella term for waveform functional/timing simulation etc.

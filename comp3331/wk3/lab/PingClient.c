334da41bf8b202ba13b44aad343b0751b1c0f241
Ryan McClue <re.mcclue@protonmail.com>
2022-09-25 15:11:09 +1000

    lab server

--- comp3331/wk3/lab/PingClient.c
+++ comp3331/wk3/lab/PingClient.c
@@ -26,13 +26,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+#include <stdbool.h>
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#define WAIT_FOR_RESPONSE_MS 600
+#define PING_COUNT 15
+#define SEQ_START 3331
+
+#define INTERNAL static
 
 typedef uint32_t u32;
+typedef uint64_t u64;
+typedef float r32;
 
+// TODO(Ryan): So all of these are characters?
 typedef struct __attribute__((packed))
 {
   char ping[4];
@@ -41,60 +54,126 @@ typedef struct __attribute__((packed))
   char crlf[2]; 
 } Msg;
 
+INTERNAL u32
+get_ms(void)
+{
+  u32 result = 0;
+
+  struct timespec time_spec = {0};
+  clock_gettime(CLOCK_REALTIME, &time_spec);
+
+  result = (r32)(time_spec.tv_sec * 1000.0f) + (r32)(time_spec.tv_nsec / 1000.0f);
+
+  return result;
+}
+
 int
 main(int argc, char *argv[])
 {
-  if (argc != 3)
+  if (argc == 3)
+  {
+    char server_ip[64] = {0};
+    strncpy(server_ip, argv[1], sizeof(server_ip));
+
+    long int server_port = strtol(argv[2], NULL, 10);
+
+    int server_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_sock != -1)
+    {
+      if (fcntl(server_sock, F_SETFL, O_NONBLOCK) != -1)
+      {
+        int opt_val = 1;
+        if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (void *)&opt_val, 
+              sizeof(opt_val)) == -1)
+        {
+          fprintf(stderr, "Warning: unable to set resuable socket (%s)\n", strerror(errno));
+        }
+
+        struct sockaddr_in server_addr = {0};
+        if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) == 1)
+        {
+          server_addr.sin_family = AF_INET;
+          server_addr.sin_port = htons(server_port);
+
+          Msg msg = {0};
+          strncpy(msg.ping, "PING", sizeof(msg.ping));
+          strncpy(msg.crlf, "\r\n", sizeof(msg.crlf));
+
+          for (u32 ping_i = 0; ping_i < PING_COUNT; ++ping_i)
+          {
+            msg.seq_num = (SEQ_START + ping_i);
+            msg.time = get_ms();
+
+            int send_len = sendto(server_sock, &msg, sizeof(msg), 0, 
+                (struct sockaddr *)&server_addr, sizeof(server_addr));
+            // TODO(Ryan): Should we retry sending data?
+            if (send_len == -1)
+            {
+              fprintf(stderr, "Error: sendto failed (%s)\n", strerror(errno));
+            }
+
+
+            u32 wait_time_ms = 0;
+            int recv_len = 0;
+            char recv_buf[64] = {0};
+            u32 start_ms = get_ms();
+            bool got_response = false;
+            while (wait_time_ms < WAIT_FOR_RESPONSE_MS)
+            {
+              // IMPORTANT(Ryan): No need to distinguish src with recvfrom()
+              recv_len = recv(server_sock, recv_buf, sizeof(recv_buf), 0);
+              if (recv_len == -1)
+              {
+                if (errno == EAGAIN)
+                {
+                  u32 cur_ms = get_ms();
+                  wait_time_ms += (cur_ms - start_ms);
+                  start_ms = cur_ms;
+                }
+                else
+                {
+                  fprintf(stderr, "Error: recv() call failed (%s)\n", strerror(errno));
+                }
+              }
+              else
+              {
+                got_response = true;
+                break;
+              }
+            }
+
+            if (got_response)
+            {
+              printf("ping to %s, seq = %d, rtt = %d ms\n", server_ip, ping_i + 1, wait_time_ms);
+            }
+            else
+            {
+              printf("ping to %s, seq = %d, rtt = 'time out'\n", server_ip, ping_i + 1);
+            }
+
+          }
+        }
+        else
+        {
+          fprintf(stderr, "Error: invalid server IP provided (%s)\n", strerror(errno));
+        }
+      }
+      else
+      {
+        fprintf(stderr, "Error: unable to set non-blocking socket (%s)\n", strerror(errno));
+      }
+    }
+    else
+    {
+      fprintf(stderr, "Error: unable to open socket (%s)\n", strerror(errno));
+    }
+
+
+  }
+  else
   {
     fprintf(stderr, "Usage: ./PingClient <server> <port>\n");
   }
-
-  char server_ip[64] = {0};
-  strncpy(server_ip, argv[1], sizeof(server_ip));
-
-  long int server_port = strtol(argv[2], NULL, 10);
-
-#define PING_COUNT 15
-#define SEQ_START 3331
-#define WAIT_FOR_RESPONSE_MS 600
-
-  // set non-blocking
-  int server_sock = socket(AF_INET, SOCK_DGRAM, 0);
-  if (server_sock == -1)
-  {
-    fprintf(stderr, "Error: unable to open socket (%s)\n", s);
-  }
-
-    int opt_val = 1;
-    if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (void *)&opt_val, sizeof(opt_val)) == -1)
-    {
-      EBP();
-    }
-
-    struct sockaddr_in server_addr = {0};
-    inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(server_port);
-
-  server_socket = ;
-
-  Msg msg = {0};
-  msg.ping = "PING";
-  msg.crlf = "\r\n";
-
-  for (u32 ping_i = 0; ping_i < PING_COUNT; ++ping_i)
-  {
-    msg.seq_num = ping_i;
-    msg.time = clock_gettime();
-
-    int len = sendto(server_sock, &msg, sizeof(msg), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    u32 wait_time_ms = 0;
-
-    n = recvfrom(server_sock, buffer, len, 0, &from, &length);
-    // time sent is ms?
-  }
-    close(sock);
 
 
   return 0;

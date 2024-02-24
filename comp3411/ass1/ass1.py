da17ba2d995aa273f626b3d33721dc40a2ec8e7d
Ryan McClue <re.mcclue@protonmail.com>
2024-02-24 13:24:32 +1100

    j

---
+++ comp3411/ass1/ass1.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+# SPDX-License-Identifier: zlib-acknowledgement
+
+import pathlib
+import os
+import sys
+import subprocess
+import logging
+import platform
+import math
+
+from dataclasses import dataclass
+
+global global_logger
+
+def fatal_error(msg):
+  global_logger.critical(msg)
+  breakpoint()
+  sys.exit()
+
+def warn(msg):
+  global_logger.warning(msg)
+  # NOTE(Ryan): Disable by passing -O to interpreter
+  if __debug__:
+    breakpoint()
+    sys.exit()
+
+def trace(msg):
+  if __debug__:
+    global_logger.debug(msg)
+
+
+def clamp(val, limit):
+  clamped_val = val
+
+  if val < 0:
+    clamped_val = 0
+  elif val >= limit:
+    clamped_val = limit - 1
+  
+  return clamped_val
+
+def read_entire_file(name):
+  res = ""
+  try:
+    f = open(name, 'r')
+    res = f.read()
+    f.close()
+  except Exception as e:
+    warn(f"Unable to read file {name}.\n{e}")
+  return res
+
+@dataclass
+class Node:
+  lim: int = 0
+  left: int = 0
+  right: int = 0
+  up: int = 0
+  down: int = 0
+
+def parse_hashi_str(hashi_str):
+  lines = hashi_str.splitlines()
+  cols = len(lines[0])
+  rows = len(lines)
+
+  locs = [][]
+
+  for line in lines
+    for i in range(len(line)):
+      ch = line[i]
+      if ch.isdigit():
+
+
+def main():
+  hashi_str = read_entire_file("hashi.puzzle")
+  parse_hashi_str(hashi_str)
+
+if __name__ == "__main__":
+  # NOTE(Ryan): Disable breakpoints if not running under a debugger
+  if sys.gettrace() is None:
+    os.environ["PYTHONBREAKPOINT"] = "0"
+  directory_of_running_script = pathlib.Path(__file__).parent.resolve()
+  os.chdir(directory_of_running_script)
+
+  global_logger = logging.getLogger(__name__)
+  global_logger.setLevel(logging.DEBUG)
+  global_logger_handler = logging.StreamHandler()
+  global_logger_handler.setLevel(logging.DEBUG)
+  global_logger_formatter = logging.Formatter('%(asctime)s - %(name)s%(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S%p')
+  global_logger_handler.setFormatter(global_logger_formatter)
+  global_logger.addHandler(global_logger_handler)
+
+  trace(f"python: {platform.python_version()}")
+
+  main()
+

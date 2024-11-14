52541301b2d9c83bf3a01b3d38e48c5aa52a9fed
Ryan McClue <re.mcclue@protonmail.com>
2024-11-14 17:23:56 +1100

    p

--- comp3222/lab7/p2-other.vhd
+++ comp3222/lab7/p2-other.vhd
@@ -36,58 +36,55 @@ ARCHITECTURE Behavior OF l7p2 IS
     -- any other signals
     SIGNAL low_reg: STD_LOGIC_VECTOR(4 DOWNTO 0);
     SIGNAL high_reg: STD_LOGIC_VECTOR(4 DOWNTO 0);
+
 	
-    TYPE State_type IS (InitLow, InitHigh, MemoryRequest, DataInspect, Finish); -- your states
+    TYPE State_type IS (InitLow, InitHigh, MemoryRequest, DataStore, DataInspect, Finish); -- your states
     SIGNAL y, y_next : State_type ;
 	
     SIGNAL address : STD_LOGIC_VECTOR(4 DOWNTO 0);
     SIGNAL data_out : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
-    SIGNAL index_bus : STD_LOGIC_VECTOR(4 DOWNTO 0);
-    SIGNAL index_enables : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    SIGNAL data_reg: STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL data_enable: STD_LOGIC;
+
+    SIGNAL buss : STD_LOGIC_VECTOR(4 DOWNTO 0);
+    SIGNAL enables : STD_LOGIC_VECTOR(1 DOWNTO 0);
 
     SIGNAL address_shift : STD_LOGIC_VECTOR(4 DOWNTO 0);
     
     SIGNAL init: STD_LOGIC;
     SIGNAL low: STD_LOGIC;
 BEGIN
-    -- TODO: what is a hardware handshake in this code?
-    -- TODO: distinction between ASM charts?
-    -- TODO: difference betwee behavioural and structural?
-
     low <= '1';
     -- your code
-    lowreg: regne PORT MAP (index_bus, index_enables(0), low, Clock, low_reg);
-    highreg: regne PORT MAP (index_bus, index_enables(1), low, Clock, high_reg);
+    lowreg: regne PORT MAP (buss, enables(0), low, Clock, low_reg);
+    highreg: regne PORT MAP (buss, enables(1), low, Clock, high_reg);
+    datareg: regne PORT MAP (data_out, data_enable, low, Clock, data_reg);
     
-    -- TODO(Ryan): Seems need to add y?
     addresscalc: PROCESS(low_reg, high_reg)
     BEGIN
+      -- address <= (high_reg + low_reg)) >> 1;
       address_shift <= (high_reg + low_reg);
-      
       address <= "0" & address_shift(4 DOWNTO 1);
-      --address <= address_shift;
 
       Addr <= address;
-      -- address <= (high_reg + low_reg)) >> 1;
     END PROCESS;
 	
     mem_blk: blk_mem_gen_0 PORT MAP (clka => Clock, addra => address, douta => data_out);
     
-    muxes: WITH index_enables & init SELECT
-      index_bus <= 
-                  "11111" WHEN "101", -- high, init (set to 31)
-                  "00000" WHEN "011", -- low, init (set to 0)
-                  address - 1 WHEN "100", -- high
-                  address + 1 WHEN "010", -- low
-                  "00000" WHEN OTHERS;
+    muxes: WITH enables & init SELECT
+      buss <= "11111" WHEN "101", -- high, init (set to 31)
+              "00000" WHEN "011", -- low, init (set to 0)
+              address WHEN "100", -- high IMPORTANT: don't do subtraction for edge case overflow
+              address + 1 WHEN "010", -- low,
+              "00000" WHEN OTHERS;
                   
     -- IMPORTANT: use combinatorial blocks for circuit 
     -- IMPORTANT: only enables with states
     -- TODO: is the general rule of if equating then put in sensitivity?
     -- is there really any logic issues that could arise if just go whole hog and put everything in unecessarily?
     -- TODO: what if put an input in this list like Data that is static?
-    statetable: PROCESS(y, s, data_out, high_reg, low_reg, Data)
+    statetable: PROCESS(y, s, data_reg, high_reg, low_reg, Data)
     BEGIN
         CASE y IS
             WHEN InitLow =>
@@ -97,9 +94,11 @@ BEGIN
                     y_next <= MemoryRequest; 
                 END IF; 
             WHEN MemoryRequest =>
+              y_next <= DataStore;
+            WHEN DataStore =>
               y_next <= DataInspect;
             WHEN DataInspect =>
-              IF (data_out = Data OR low_reg >= high_reg) THEN
+              IF (data_reg = Data OR low_reg = high_reg) THEN
                 y_next <= Finish;
               ELSE
                 y_next <= MemoryRequest;
@@ -111,31 +110,34 @@ BEGIN
         END CASE;
     END PROCESS;
 
-    controlsignals: PROCESS(y, data_out, Data, high_reg)
+    controlsignals: PROCESS(y, data_reg, Data)
     BEGIN
        -- Clear all outputs
        Done <= '0'; 
        Found <= '0'; 
-       index_enables <= "00";
+       enables <= "00";
        init <= '0';
+       data_enable <= '0';
 
        CASE y IS
           WHEN InitLow =>
-            index_enables(0) <= '1';
+            enables(0) <= '1';
             init <= '1';
           WHEN InitHigh =>
-            index_enables(1) <= '1';
+            enables(1) <= '1';
             init <= '1';
           WHEN MemoryRequest =>
+          WHEN DataStore =>
+             data_enable <= '1';
           WHEN DataInspect =>
-             IF (data_out < Data) THEN
-               index_enables(0) <= '1'; -- change low to addr + 1
-             ELSIF (data_out > Data) THEN
-               index_enables(1) <= '1'; -- change high to addr - 1
+             IF (data_reg < Data) THEN
+               enables(0) <= '1'; -- change low to addr + 1
+             ELSIF (data_reg > Data) THEN
+               enables(1) <= '1'; -- change high to addr
              END IF;
           WHEN Finish =>
             Done <= '1';
-            IF (data_out = Data) THEN
+            IF (data_reg = Data) THEN
               Found <= '1';
             END IF;
        END CASE;
---
+++ comp3222/lab7/psuedo.md
@@ -0,0 +1,18 @@
+let high = 31;
+let low = 0;
+let done = False;
+
+while not done:
+  address = (high + low) / 2;
+  search = memory(address); 
+
+  if search == data:
+    done = True;
+    found = True;
+  else if low == high:
+    done = True;
+    found = False;
+  else if search < data:
+    low = address + 1
+  else:
+    high = address
